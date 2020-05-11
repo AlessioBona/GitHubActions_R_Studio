fd9847d62bd6ca02d490991c83b1379924ff7660
SelinaZitrone <selina.baldauf@gmx.de>
2020-05-11 15:39:51 +0200

    initial commit

---
+++ setup.R
@@ -0,0 +1,14 @@
+# associate a .git folder with your R-project (if you have not already done that)
+usethis::use_git()
+# create a github project for your local R-project (https://happygitwithr.com/)
+usethis::use_github()
+
+# this will add a .github/workflows folder with a R-CMD-check.yaml file to your project
+# also it will create a .Rbuildignore to ignore the .github folder when building your project
+
+usethis::use_github_actions()
+
+#usethis::use_github_actions_badge(name = "R-CMD-check")
+
+# create a folder with tests using testthat package
+usethis::use_testthat()
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(GitHubActions_R_Studio)
+
+test_check("GitHubActions_R_Studio")
